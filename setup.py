b1252d13fa598f3494cabdd5f9cdc0ba4e9fa506
Rui Luis <ruisergioluis@gmail.com>
2017-12-02 02:42:37 +0000

    Updated Setup file

--- setup.py
+++ setup.py
@@ -2,24 +2,24 @@ import sys
 from setuptools import setup
 
 setup(
-    name = "helloworld",        # what you want to call the archive/egg
+    name = "BamInsight",        # what you want to call the archive/egg
     version = "0.1",
-    packages=["helloworld"],    # top-level python modules you can import like
+    packages=["Main"],    # top-level python modules you can import like
                                 #   'import foo'
     dependency_links = [],      # custom links to a specific project
     install_requires=[],
     extras_require={},      # optional features that other packages can require
                             #   like 'helloworld[foo]'
     package_data = {},
-    author="David Barnett",
-    author_email = "davidbarnett2@gmail.com",
-    description = "The familiar example program in Python",
+    author="Rui Luis",
+    author_email = "ruisergioluis@gmail.com",
+    description = "A Useful bioinformatic tool to quickly vizualize your data in Genome Browser UCSC",
     license = "BSD",
-    keywords= "example documentation tutorial",
-    url = "http://github.com/dbarnett/python-helloworld",
+    keywords= "Genome Bowser UCSC tool",
+    url = "https://github.com/rluis/BamInsight",
     entry_points = {
         "console_scripts": [        # command-line executables to expose
-            "helloworld = helloworld.main:main",
+            "baminsight = Main.main:main",
         ],
         "gui_scripts": []       # GUI executables (creates pyw on Windows)
     }
