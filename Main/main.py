d6e19e13f9ef704b2d9ad804fb4b9aac1a98797c
Rui Luis <ruisergioluis@gmail.com>
2017-12-05 11:28:24 +0000

    Start to develop Bam handling module

--- Main/main.py
+++ Main/main.py
@@ -1,36 +1,41 @@
 import sys
 import inputs
-import Configs.configs as conf
 
-#Main Function of the program. This is the entry point to the software
 
-# Accept the arguments given by the user
-args = inputs.entry_inputs()
 
-# Checking if Genome is allowed
-sys.stdout.write("Checking if your Genome is allowed:")
-inputs.genomeCheck(args.genome)
-sys.stdout.write(" CHECK!\n")
+#This function call inputs fucntion, and then check the arguments given
+def argsNcheckers():
+    # Accept the arguments given by the user
+    args = inputs.entry_inputs()
 
-# Checking Bam Files
-sys.stdout.write("Checking Files Constraints:")
-inputs.bamNumberCheck(args.name)
-inputs.checkFilesSource(args.name)
-args.basename = inputs.defineBasenamesBAMs(args.name)
-
-# Checking Long Labels
-args.long_label = inputs.longLabelCopyFromBasename(args.long_label,args.basename)
-inputs.bamNLongLabelNumberConcordance(args.long_label,args.name)
-
-#Checking Short Labels
-args.short_label = inputs.shortLabelCopyFromLongLabel(args.short_label,args.long_label)
-inputs.LongLabelShortLabelNumberConcordance(args.short_label,args.long_label)
-
-sys.stdout.write(" CHECK!\n")
-
-# Checking FTP Server
-if inputs.isFTPServerGiven(args.host_FTP):
-    sys.stdout.write("Checking FTP Server Connection:")
-    inputs.checkConnectionFTP(args.host_FTP)
+    # Checking if Genome is allowed
+    sys.stdout.write("Checking if your Genome is allowed:")
+    inputs.genomeCheck(args.genome)
     sys.stdout.write(" CHECK!\n")
 
+    # Checking Bam Files
+    sys.stdout.write("Checking Files Constraints:")
+    inputs.bamNumberCheck(args.name)
+    inputs.checkFilesSource(args.name)
+    args.basename = inputs.defineBasenamesBAMs(args.name)
+
+    # Checking Long Labels
+    args.long_label = inputs.longLabelCopyFromBasename(args.long_label,args.basename)
+    inputs.bamNLongLabelNumberConcordance(args.long_label,args.name)
+
+    #Checking Short Labels
+    args.short_label = inputs.shortLabelCopyFromLongLabel(args.short_label,args.long_label)
+    inputs.LongLabelShortLabelNumberConcordance(args.short_label,args.long_label)
+
+    sys.stdout.write(" CHECK!\n")
+
+    # Checking FTP Server
+    if inputs.isFTPServerGiven(args.host_FTP):
+        sys.stdout.write("Checking FTP Server Connection:")
+        inputs.checkConnectionFTP(args.host_FTP)
+        sys.stdout.write(" CHECK!\n")
+
+    return args
+
+def createBWFromBAM(BAMList, BasenameList, Flags_F = None, Flags_R = None):
+    BAMList
---
+++ Main/system.py
@@ -0,0 +1,14 @@
+##########################################################################
+##                                                                      ##
+##                      SYSTEM CONTROL SOFTWARE PAGE                    ##
+##                                                                      ##
+##########################################################################
+
+# Modules Required from BamInsight
+import main
+
+
+def system():
+    # accept the inputs and check their reliability
+    args = main.argsNcheckers()
+
